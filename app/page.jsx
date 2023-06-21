80491d86992289ef4f0f6487a01599cb3f3a70df
Yannick Schläpfer <yannick.schlaepfer@gibwintra.ch>
2023-06-21 15:30:00 +0200

    landing page links centered and font edited

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Home({ params }) {
   return (
     <main>
-      <h1 className="landingPagge">Choose one of the following Options</h1>
+      <h1 className="">Choose one of the following Options</h1>
       <div className="landingPage">
         <Link className="createGroup" href="/create">Create Group</Link>
         <Link className="joinGroup" href="/join">Join Group</Link>
