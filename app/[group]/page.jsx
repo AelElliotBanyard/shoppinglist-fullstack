b97a1a86fd79babc98759cefd86e59bb2a0d35dc
sujan_Saravana <sujan.saravana@lernende.bbw>
2023-06-28 09:12:25 +0200

    Icons for add item and delete group

--- app/[group]/page.jsx
+++ app/[group]/page.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ListItem from "../../components/ListItem";
+import { FaCog, FaPlus } from 'react-icons/fa'
+import Link from 'next/link';
 
 const GroupPage = ({ params }) => {
   return (
     <div className="groupPage">
-      <h1 className="groupTitle">title</h1>
+      <div className="groupHeader">
+        <Link href={`/${params.group}/item/new`} className="addItem"><FaPlus size={24} /></Link>
+        <h1 className="groupTitle">title</h1>
+        <Link href={`/${params.group}/settings`} className="groupSettings"><FaCog size={24} /></Link>
+      </div>
       <div className="groupList">
         <ListItem id="13413" group_id={params.group} title="bananas" />
       </div>
