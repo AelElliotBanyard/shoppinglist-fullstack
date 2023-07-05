bfc68d024ef17293e3d3b4152c4700a6824619a4
Evan <evan.lueber@stud.kbw.ch>
2023-07-05 13:41:52 +0200

    styling optimized

--- app/page.jsx
+++ app/page.jsx
@@ -2,9 +2,9 @@ import Link from 'next/link'
 
 export default function Home({ params }) {
   return (
-    <main>
-      <h1 className="">Choose one of the following Options</h1>
-      <div className="landingPage">
+    <main className='landingPage'>
+      <h1 className="choiceText">Choose one of the following Options</h1>
+      <div className="landingPageOptions">
         <Link className="CJGroup" href="/create">Create Group</Link>
         <Link className="CJGroup" href="/join">Join Group</Link>
         </div>
