671650234ad0158a7b30400c0b820d7b20d8a4c8
Evan <evan.lueber@stud.kbw.ch>
2023-07-05 13:34:29 +0200

    styled for mobile

--- app/[group]/item/[id]/page.jsx
+++ app/[group]/item/[id]/page.jsx
@@ -1,17 +1,28 @@
-'use client'
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import { getItem, deleteItem } from "@/lib/databaseItem";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
+
+const getItemData = async (id) => {
+  const data = await getItem(id);
+  return data;
+};
 
 const ItemPage = async ({ params }) => {
-  const {id} = params;
-  const data = await getItem(id);
+  const { id } = params;
+  let data = {};
   let description = data.description;
   const changeDescription = (e) => {
-    description = (e.target.value);
+    description = e.target.value;
   };
-  const router = useRouter()
+  const router = useRouter();
 
+  useEffect(() => {
+    const fetchData = async () => {
+      data = await getItemData(id);
+    };
+    fetchData();
+  }, []);
 
   return (
     <div className="itemPage">
@@ -21,22 +32,30 @@ const ItemPage = async ({ params }) => {
       <div className="itemPageValue">
         <div className="itemDescription">
           <h2 className="itemSubtitle">Description</h2>
-          <input type="text" className="descriptionInput" value={description} onChange={changeDescription}/>
+          <input
+            type="text"
+            className="descriptionInput"
+            value={description}
+            onChange={changeDescription}
+          />
         </div>
-        <div className="rightSideItem">
-          <div>
-            <h2 className="itemSubtitle">Created By</h2>
-            <p>{data.created_by}</p>
-          </div>
-          <div>
-            <h2 className="itemSubtitle">Assigned To</h2>
-            <p>{data.assigned_to}</p>
-          </div>
-          <button className="deleteItem" onClick={() => {
+        <div>
+          <h2 className="itemSubtitle">Created By</h2>
+          <p>{data.created_by}</p>
+        </div>
+        <div>
+          <h2 className="itemSubtitle">Assigned To</h2>
+          <p>{data.assigned_to}</p>
+        </div>
+        <button
+          className="deleteItem"
+          onClick={() => {
             deleteItem(id);
             router.back();
-          }}>Delete</button>
-        </div>
+          }}
+        >
+          Delete
+        </button>
       </div>
     </div>
   );
